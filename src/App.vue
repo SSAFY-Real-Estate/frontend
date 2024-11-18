068d76ba9be383773553c379f9db414f2d6e6b45
이동규 <winlouder3@gmail.com>
2024-11-18 13:57:39 +0900

    저장용

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <script setup>
-import StartView from './views/StartView.vue';
+import BoardListView from "./views/BoardListView.vue";
+import StartView from "./views/StartView.vue";
 </script>
 
 <!-- 1920 X 1080 해상도 기준 -->
 <template>
-    <div id="wrap">
-        <StartView />
-    </div>
+  <div id="wrap">
+    <!-- <StartView /> -->
+    <BoardListView />
+  </div>
 </template>
 
 <style>
-    #wrap {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    }
+#wrap {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
 </style>
---
+++ src/components/board/BoardCard.vue
@@ -0,0 +1,68 @@
+<script setup></script>
+
+<template>
+  <div class="BoardCard">
+    <div class="profileImg">
+      <!-- <div><img src="../../assets/houseLogo.png" /></div> -->
+    </div>
+    <div class="content">
+      <div class="content_child">
+        <div class="content_child_title"></div>
+        <div class="content_child_content"></div>
+        <div class="content_child_date"></div>
+      </div>
+      <div>제목</div>
+      <div>내용</div>
+      <div>날짜 + 댓글 수</div>
+    </div>
+    <div></div>
+  </div>
+</template>
+
+<style scoped>
+.BoardCard {
+  box-sizing: border-box;
+  border-radius: 15px;
+  width: 272px;
+  height: 272px;
+  border: 1px solid black;
+}
+
+.content {
+  padding: 10px;
+  width: 100%;
+  height: 40%;
+  background-color: green;
+  box-sizing: border-box;
+  border-bottom: 1px solid black;
+}
+/* .content_child {
+  margin: 3px;
+  background-color: green;
+  width: 100%;
+  height: 100%;
+} */
+/* .content_child_title {
+  width: 100%;
+  height: 40%;
+  background-color: aqua;
+}
+.content_child_content {
+  width: 100%;
+  height: 40%;
+  background-color: blueviolet;
+}
+
+.content_child_date {
+  width: 100%;
+  height: 20%;
+  background-color: blue;
+} */
+
+.profileImg {
+  box-sizing: border-box;
+  border-bottom: 1px solid black;
+  width: 100%;
+  height: 45%;
+}
+</style>
