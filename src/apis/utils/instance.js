c568ae7339677e6bb132ea25238224cfc3e9d824
seowoo <dkr0210@naver.com>
2024-11-21 09:13:17 +0900

    docs: 병합 충돌 코드 수정

--- src/apis/utils/instance.js
+++ src/apis/utils/instance.js
@@ -2,18 +2,11 @@ import axios from "axios";
 import getServerAddress from "@/constants/serverAddress";
 
 export const instance = axios.create({
-<<<<<<< HEAD
     baseURL: "http://" + getServerAddress(),
     headers: {
-        Authorization: "Bearer " + localStorage.getItem("AccessToken")
+        Authorization: "Bearer " + localStorage.getItem("AccessToken"),
+        "Content-Type": "application/json;charset=utf-8",
     }
 })
-=======
-  baseURL: "http://" + getServerAddress(),
-  headers: {
-    "Content-Type": "application/json;charset=utf-8",
-  },
-});
->>>>>>> map
 
 export default instance;
