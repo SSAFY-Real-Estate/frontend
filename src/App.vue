621da7511b5fe27b4867536e476ae4b1de1b962b
Lee <winlouder3@gmail.com>
2024-11-24 03:38:57 +0900

    for store

--- src/App.vue
+++ src/App.vue
@@ -18,18 +18,18 @@ import { jwtDecode } from "jwt-decode";
 const route = useRouter();
 
 onMounted(() => {
-  const token = localStorage.getItem('AccessToken');
+  const token = localStorage.getItem("AccessToken");
   // 로그인을 한 상태로 웹브라우저 종료시 토큰이 만료하기 전까지는 로그인 상태 유지
-  // if(jwtDecode(token)) route.push({name : 'main'}); 
+  // if(jwtDecode(token)) route.push({name : 'main'});
 });
-
 </script>
 
 <!-- 1920 X 1080 해상도 기준 -->
 <template>
   <div id="wrap">
     <Header v-if="route.currentRoute.value.path !== '/'" />
-    <RouterView />
+    <!-- <RouterView /> -->
+    <MapView />
   </div>
 </template>
 
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,66 +1,53 @@
 <script setup>
 import { KakaoMap, KakaoMapMarker } from "vue3-kakao-maps";
-import { onMounted, ref, watch } from "vue";
-import { getLocation } from "@/apis/map/map.js";
-const mapContainer = ref(null);
+import { onMounted, ref, watch, onBeforeMount } from "vue";
+import { getLocation } from "@/apis/map/map";
 
-onMounted(() => {
-  loadKakaoMap(mapContainer.value);
+const corInfo = ref(); // 좌표 data
+const map = ref();
+const location = ref({
+  // getLocation parameter
+  swLat: 28.814718216888917,
+  swLng: 119.87472617492291,
+  neLat: 37.66096638568398,
+  neLng: 133.95703806681934,
 });
 
-const loadKakaoMap = (container) => {
-  const script = document.createElement("script");
-  script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${
-    import.meta.env.VITE_KAKAO_MAP_SERVICE_KEY
-  }`;
-  document.head.appendChild(script);
-
-  script.onload = () => {
-    window.kakao.maps.load(() => {
-      const options = {
-        //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-        level: 3, //지도의 레벨(확대, 축소 정도)
-      };
-
-      const mapInstance = new window.kakao.maps.Map(container, options);
-    });
-  };
+const onLoadKakaoMap = (mapRef) => {
+  map.value = mapRef;
+  console.log("map");
 };
 
-//map load
-//현재 map의 중앙값
-// const map = ref(); // 카카오 맵
+// axios
+const gLoction = () => {
+  getLocation(
+    location.value,
+    ({ data }) => {
+      // 좌표 객체들을 position 배열로 묶어서 넣기
+      corInfo.value = data;
+    },
 
-// //남동쪽 북서쪽 좌표 범위 안에 있는 좌표들을 가져옴
-// const locationList = ref([]); // 범위안 좌표
-
-// const apiGetLocation = (location) => {
-//   getLocation(
-//     location,
-//     ({ data }) => {
-//       locationList.value = data.map((item, index) => ({
-//         key: index + 1, // 고유한 key 값 추가 (1부터 시작)
-//         ...item, // 기존 데이터 그대로 유지
-//       }));
-//       const markers = data.map(
-//         ({ lat, lng }) =>
-//           new kakao.maps.Marker({
-//             position: new kakao.maps.LatLng(lat, lng),
-//           })
-//       );
-
-//       clusterer.addMarkers(markers);
-//       console.log(clusterList);
-//     },
-//     (error) => {
-//       console.log(error);
-//     }
-//   );
-// };
+    (error) => {
+      console.log(error);
+    }
+  );
+};
+onBeforeMount(() => {
+  gLoction();
+  console.log(corInfo.value);
+});
 </script>
 
 <template>
-  <div ref="mapContainer" style="width: 500px; height: 500px"></div>
+  <button @click="getInfo">누르세요</button>
+  <KakaoMap
+    :lat="33.450701"
+    :lng="126.57066"
+    :markerCluster="{
+      markers: corInfo,
+    }"
+    :level="14"
+    @onLoadKakaoMap="onLoadKakaoMap"
+  />
 </template>
 <style scoped></style>
