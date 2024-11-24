b4a593c2ab511b5a389941da647d0cdfa5430239
이동규 <winlouder3@gmail.com>
2024-11-24 13:42:06 +0900

    for merge

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -2,8 +2,9 @@
 import { KakaoMap, KakaoMapMarker } from "vue3-kakao-maps";
 import { onMounted, ref, watch, onBeforeMount } from "vue";
 import { getLocation } from "@/apis/map/map";
+import { isKakaoMapApiLoaded } from 'vue3-kakao-maps/@utils';
 
-const corInfo = ref(); // 좌표 data
+const corInfo = ref([]); // 좌표 data
 const map = ref();
 const location = ref({
   // getLocation parameter
@@ -12,11 +13,6 @@ const location = ref({
   neLat: 37.66096638568398,
   neLng: 133.95703806681934,
 });
-
-const onLoadKakaoMap = (mapRef) => {
-  map.value = mapRef;
-  console.log("map");
-};
 
 // axios
 const gLoction = () => {
@@ -32,10 +28,39 @@ const gLoction = () => {
     }
   );
 };
-onBeforeMount(() => {
-  gLoction();
-  console.log(corInfo.value);
+
+const onLoadKakaoMap = (mapRef) => {
+  map.value = mapRef;
+  console.log("map");
+};
+
+const getInfo = () => {
+  if (map.value) {
+    const bounds = map.value.getBounds();
+    const swLatLng = bounds.getSouthWest();
+    const neLatLng = bounds.getNorthEast();
+    location.value.swLat = swLatLng.getLat();
+    location.value.swLng = swLatLng.getLng();
+    location.value.neLat = neLatLng.getLat();
+    location.value.neLng = neLatLng.getLng();
+    gLoction();
+  }
+}
+
+watch(map, () => {
+  if (map.value && isKakaoMapApiLoaded.value) {
+    kakao.maps.event.addListener(map.value, "dragend", () => {
+      getInfo();
+    });
+
+    kakao.maps.event.addListener(map.value, "zoom_changed", () => {
+      const level = map.value.getLevel(); // 변경된 줌 레벨 가져오기
+      console.log(`지도 줌 레벨이 변경되었습니다: ${level}`);
+    });
+  }
 });
+
+
 </script>
 
 <template>
@@ -43,11 +68,9 @@ onBeforeMount(() => {
   <KakaoMap
     :lat="33.450701"
     :lng="126.57066"
-    :markerCluster="{
-      markers: corInfo,
-    }"
-    :level="14"
+    :markerList="corInfo"
     @onLoadKakaoMap="onLoadKakaoMap"
   />
+  <div>{{corInfo}}</div>
 </template>
 <style scoped></style>
